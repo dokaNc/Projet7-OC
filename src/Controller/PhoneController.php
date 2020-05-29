b8d568f6a14100acfc14d9e543d9c6212e359a38
Dogukan <cirpan.dogukan5959@gmail.com>
2020-05-29 12:22:46 +0200

    Write the base of the Controller

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * @Route("/api")
+ */
 class PhoneController extends AbstractController
 {
     /**
-     * @Route("/phone", name="phone")
+     * @Route("/phones/", name="list_phone", methods={"GET"})
+     * @param PhoneRepository $phoneRepository
+     * @param SerializerInterface $serializer
+     * @return Response
      */
-    public function index()
+    public function index(PhoneRepository $phoneRepository, SerializerInterface $serializer)
     {
-        return $this->render('phone/index.html.twig', [
-            'controller_name' => 'PhoneController',
+        $phones = $phoneRepository->findAll();
+        $data = $serializer->serialize($phones, 'json');
+
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
         ]);
     }
 }
