cd67c8d6ec3d7deadfeea053213535f7614793ae
Dogukan <cirpan.dogukan5959@gmail.com>
2020-06-19 13:40:57 +0200

    Install package "WhiteOctoberPagerfantaBundle"

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
 ];
