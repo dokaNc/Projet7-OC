0b1bc3498074b27e0e11d4f99317166453e01659
Dogukan <cirpan.dogukan5959@gmail.com>
2020-08-07 12:50:03 +0200

    Add function with GET method for show all user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 /**
  * Class ClientController
  * @package App\Controller
+ * @Route("/api")
  */
 
-class UserController
+class UserController extends AbstractController
 {
+    private $entityManager;
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $repository)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Rest\Get(
+     *     path = "/user/{id}",
+     *     name = "app_user_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @param User $user
+     * @return View
+     */
+    public function showAction(User $user)
+    {
+        return View::create($user, Response::HTTP_ACCEPTED);
+    }
 }
