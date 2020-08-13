07e17482db584cf9a5d54d3ed6e69b9bacbf23d8
Dogukan <cirpan.dogukan5959@gmail.com>
2020-08-13 08:11:36 +0200

    add new User link to Admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,10 +7,13 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class ClientController
@@ -25,11 +28,16 @@ class UserController extends AbstractController
      * @var UserRepository
      */
     private $repository;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
 
-    public function __construct(EntityManagerInterface $entityManager, UserRepository $repository)
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $repository, UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->entityManager = $entityManager;
         $this->repository = $repository;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
     /**
@@ -44,6 +52,39 @@ class UserController extends AbstractController
     public function showAction(User $user)
     {
         return View::create($user, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @Rest\Post(
+     *    path = "/admin/add/user",
+     *    name = "app_admin_add_user"
+     * )
+     * @ParamConverter(
+     *     "newUser",
+     *     converter="fos_rest.request_body",
+     *     options={
+     *         "validator"={ "groups"="Register" }
+     *     }
+     * )
+     * @IsGranted("ROLE_ADMIN")
+     * @param User $newUser
+     * @return View
+     */
+    public function addUser(User $newUser)
+    {
+        $user = new User();
+
+        $user->setEmail($newUser->getEmail());
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($newUser, ($newUser->getPassword()))
+        );
+        $user->setClients($this->getUser()->getClients());
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return View::create($user, Response::HTTP_OK);
     }
 
     /**
